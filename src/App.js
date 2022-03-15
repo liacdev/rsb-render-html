ed82c5ef47c8b6ea8b0a24eb0c962d7e5ea91615
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-03-15 13:37:53 -0500

    functional component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import RenderHTML from "./Components/render-html.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>React Render HTML</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/SouthpawCurveConcepts/rsb-render-html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link to repo
         </a>
+        <RenderHTML />
       </header>
     </div>
   );
---
+++ src/Components/render-html.js
@@ -0,0 +1,13 @@
+function RenderHTML() {
+  return (
+    <div>
+      <h2>React Render HTML</h2>
+      <p>
+        React's goal is in many ways to render HTML in a web page. React renders
+        HTML to the web page by using a function called ReactDOM.render().
+      </p>
+    </div>
+  );
+}
+
+export default RenderHTML;
